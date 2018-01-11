4fbb0542cb26bd5cc55b6d22d9e4302bb89f7d67
dudor <dudor@qq.com>
2018-01-11 15:29:28 +0800

    add loginpage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import dva from 'dva';
 import './index.css';
+import createLoading from 'dva-loading'
+import { browserHistory } from 'dva/router'
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  history: browserHistory,
+  onError(error) {
+    console.log("app error:", error);
+  }
+});
 
 // 2. Plugins
 // app.use({});
+app.use(createLoading({ effected: true }));
 
 // 3. Model
 // app.model(require('./models/example').default);
